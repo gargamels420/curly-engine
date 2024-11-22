1cc2c135770e5eab21047135989fa681bb69de5a
Gargamel <135997954+gargamels420@users.noreply.github.com>
2024-11-22 20:32:15 +0100

    Update game.js

--- routes/game.js
+++ routes/game.js
@@ -3,15 +3,32 @@ const router = express.Router();
 var gameField = ["","","",
                  "","","",
                  "","",""];
+var xField = [];
+var oField = [];
+var win = false;
 
 // Simulate a table update API
 router.post('/table', (req, res) => {
     const { value } = req.body;
     gameField[value] = "x";
+    xField.push(value)
     console.log(gameField);
+    if (xField.length <= 2 ) {
+        check();
+    }
     
     // Simulate a successful response
     res.json({ message: 'Table updated successfully!' });
 });
+
+async function check(value) {
+    if (xField[value-1]=="x") {
+        if (xField[value-2]=="x") {
+            win = true;
+        } else if (xField[value+1]=="x") {
+            win = true;
+        }
+    }
+}
 
 module.exports = router;
