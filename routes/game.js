b8d187c6be9f2b37033dc9a14e6ef0cebb34c8d0
Gargamel <135997954+gargamels420@users.noreply.github.com>
2024-11-26 15:40:04 +0100

    Update game.js

--- routes/game.js
+++ routes/game.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-let gameField = ["","","",
-                 "","","",
-                 "","",""];
-let xField = [];
-let oField = [];
+let gameField = [["", "", ""],
+["", "", ""],
+["", "", ""]];
+let tahy = 0;
 let win = false;
 
 // Simulate a table update API
@@ -12,38 +11,95 @@ router.post('/table', (req, res) => {
     const { row } = req.row;
     const { column } = req.column;
     gameField[row][column] = "x";
-    xField.push(value)
-    console.log(gameField);
-    if (xField.length >= 2 ) {
-        win = "ha";
-        check(row,column);
+    tahy = taky + 1;
+    if (tahy >= 2) {
+        checkColumn(row, column);
     }
-    
+
 
     // Simulate a successful response
     res.json({ message: win });
 });
 
-async function check(row,column) {
-  if (va+1 % 3==0) {
-    if (gameField[va-1]=="x") {
-        if (gameField[va-2]=="x") {
-            win = true;
-        } 
+async function checkColumn(row, column) {
+    if (column == 2) {
+        if (gameField[row][column - 1] == "x") {
+            if (gameField[row][column - 2] == "x") {
+                win = true;
+            }
+        }
     }
+    else if (column == 1) {
+        if (gameField[row][column + 1] == "x") {
+            if (gameField[row][column - 1] == "x") {
+                win = true;
+            }
+        }
     }
-  else if (va+1 % 3==0) {
-  if (gameField[va+1]=="x"){
-     if (gameField[va-1]=="x") {
-            win = true;
-        } 
-        } 
+    else if (column == 0) {
+        if (gameField[row][column + 1] == "x") {
+            if (gameField[row][column + 2] == "x") {
+                win = true;
+            }
+        }
     }
-  if (gameField[va+1]=="x"){
-     if (gameField[va+2]=="x") {
-            win = true;
-        } 
-        } 
+    else {
+        checkRow(row, column);
+    }
+}
+
+async function checkRow(row, column) {
+    if (row == 2) {
+        if (gameField[row - 1][column] == "x") {
+            if (gameField[row - 2][column] == "x") {
+                win = true;
+            }
+        }
+    }
+    else if (row == 1) {
+        if (gameField[row + 1][column] == "x") {
+            if (gameField[row - 1][column] == "x") {
+                win = true;
+            }
+        }
+    }
+    else if (row == 0) {
+        if (gameField[row + 1][column] == "x") {
+            if (gameField[row + 2][column] == "x") {
+                win = true;
+            }
+        }
+    }
+    else {
+        checkDiagonal(row,column);
+    }
+}
+
+async function checkDiagonal(row, column) {
+    if (row == 2) {
+        if (gameField[row - 1][column] == "x") {
+            if (gameField[row - 2][column] == "x") {
+                win = true;
+            }
+        }
+    }
+    else if (row == 1) {
+        if (gameField[row + 1][column] == "x") {
+            if (gameField[row - 1][column] == "x") {
+                win = true;
+            }
+        }
+    }
+    else if (row == 0) {
+        if (gameField[row + 1][column] == "x") {
+            if (gameField[row + 2][column] == "x") {
+                win = true;
+            }
+        }
+    }
+    else {
+        checkDiagonal();
+    }
 }
 
 module.exports = router;
